e4b06935f5f67842761b9c9daa03d0d42c4660d5
Sail Somoshi <sailsomoshi11@gmail.com>
2022-01-04 16:45:57 +0530

    ADDED:Emp wage for each company

--- src/com/wage/EmployeeWageComputation.java
+++ src/com/wage/EmployeeWageComputation.java
@@ -3,19 +3,17 @@ package com.wage;
 public class EmployeeWageComputation {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_DAYS = 20;
-    public static final int MAX_HOURS_IN_MONTH = 100;
 
     public static void main(String[] args) {
-        empWageComputation();
+                empWageComputation("dmart",20,10,10);
+                empWageComputation("tcs",20,20,100);
     }
-    public static void empWageComputation() {
+    public static int empWageComputation(String company, int empRatePerHour, int numOfWorkingDays,int maxHourPerMonth) {
         System.out.println("Welcome to employee wage computation");
         int empHour = 0;
         int totalWorkingDays = 0;
         int totalEmpHours = 0;
-        while ( totalEmpHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_DAYS ) {
+        while ( totalEmpHours <= maxHourPerMonth && totalWorkingDays < numOfWorkingDays ) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() *10)%3;
             switch (empCheck) {
@@ -31,8 +29,8 @@ public class EmployeeWageComputation {
             System.out.println("day : " + totalWorkingDays + " emp hour : " + empHour );
             totalEmpHours+= empHour;
         }
-        int totalEmpWages = totalEmpHours * EMP_RATE_PER_HOUR;
-        System.out.println("total employee hours is " + totalEmpHours);
-        System.out.println("total employee wage is " + totalEmpWages);
+        int totalEmpWages = totalEmpHours * empRatePerHour;
+        System.out.println("total employee wage for company: " +company +" is: "+totalEmpWages);
+        return totalEmpWages;
     }
 }
